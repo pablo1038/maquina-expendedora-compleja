339b30bd30d867ac495411f85559632c66d65d2e
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-11-06 10:40:03 +0100

    0745.04 añadimos un nuevo atributo llamado NumeroBilletes yco esto conseguimos añadir dos nuevos metodos para saber el numero de billetes vwndidos y que imprima los billetes vendidos a su vez

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -10,7 +10,8 @@ public class MaquinaExpendedoraMejorada {
     private String estacionOrigen;
     // El destino del billete
     private String estacionDestino;
-
+    // Total de billetes vendidos desde la puesta en marcha de la maquina
+    private int numeroBilletesVendidos;
     /**
      * Crea una maquina expendedora de billetes de tren con el 
      * precio del billete y el origen y destino dados. Se asume que el precio
@@ -21,6 +22,7 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
         estacionOrigen = origen;
+        numeroBilletesVendidos = 0;
         estacionDestino = destino;
     }
     public int vaciarDineroDeLaMaquina() { 
@@ -33,8 +35,15 @@ public class MaquinaExpendedoraMejorada {
             totalDineroAcumulado = 0; 
         } 
         return valorDinero;
+    }
+    public int getnumeroBilletesVendidos() {
+        return numeroBilletesVendidos;
+    }
+    public void imprimenumeroBilletesVendidos() {
+        System.out.println("Se han imprimido" +  numeroBilletesVendidos);
+        
     }    
-    /**
+    /** 
      * Devuelve el precio del billete
      */
     public int getPrecioBillete() {
@@ -78,6 +87,8 @@ public class MaquinaExpendedoraMejorada {
             totalDineroAcumulado = totalDineroAcumulado + precioBillete;
             // Reduce el balance del cliente actual dejandole seguir utilizando la maquina
             balanceClienteActual = balanceClienteActual - precioBillete;
+            // Numero Máximo de Billetes 
+            numeroBilletesVendidos = numeroBilletesVendidos + 1;
         }
         else {
             System.out.println("Necesitas introducir " + cantidadDeDineroQueFalta + " euros mas!");
